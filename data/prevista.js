02b1d2f35f4019dbb405f29b5d01a50f654afd0a
Marcelosilvacmmatosinhos <marcelo.silva@cm-matosinhos.pt>
2025-06-16 09:00:51 +0100

    Add files via upload

---
+++ data/prevista.js
@@ -0,0 +1,9 @@
+var previstaData = {
+  "type": "FeatureCollection",
+  "name": "prevista",
+  "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  "features": [
+    {"type":"Feature","id":1,"geometry":{"type":"Point","coordinates":[-8.638611625996392,41.221751925849375]},"properties":{}},
+
+]
+  }
